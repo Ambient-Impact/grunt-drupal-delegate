f40d4b72b351b21c60d84f7e566e505afaaa440d
Matei Stanca <i@ambientimpact.com>
2022-05-23 09:27:47 -0400

    Removed unused require() from index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@
 
 module.exports = function(grunt) {
 
-  const childProcess = require('child_process');
-
-  const shescape = require('shescape');
-
   require('./lib/recursion_check')(grunt);
 
   require('./lib/drush_check')(grunt);
